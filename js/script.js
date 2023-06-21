d4ba14023eb1856765bf2551e74cb67df665716d
natypuhl <132513005+natypuhl@users.noreply.github.com>
2023-06-21 12:37:16 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+alert ("Bienvenidos")
+function inclusive()
+      {
+          document.body.style.backgroundColor= "black";
+          document.getElementById("main").style.backgroundColor = "black";
+          document.getElementById("header").style.backgroundColor = "black";
+          document.getElementById("header").style.color = "#90ffe9";
+          document.getElementById("nav").style.backgroundColor = "whitesmoke";
+          document.getElementById("nav").style.color = "black";
+          document.getElementById("section").style.backgroundColor = "black";
+          document.getElementById("section").style.color = "whitesmoke";
+          document.getElementById("aside").style.backgroundColor = "black";
+          document.getElementById("footer").style.backgroundColor = "black";
+      }
+
+      function copy() {
+          alert("Natalia Puhl , Mail : natypuhl2301@hotmail.com");
+          }
